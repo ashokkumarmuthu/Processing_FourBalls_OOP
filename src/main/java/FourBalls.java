a3c1031d8dffc26e805e20757806e13c2c49ad7c
Ashok <ashokkumar.cs18@bitsathy.ac.in>
2022-03-15 16:43:54 +0530

    Implemented with OOP

--- src/main/java/FourBalls.java
+++ src/main/java/FourBalls.java
@@ -1,47 +1,54 @@
+
 import processing.core.PApplet;
 
-public class FourBalls extends PApplet{
+import java.util.ArrayList;
 
-    public static final int WIDTH = 1280;
-    public static final int HEIGHT = 720;
-    int x=0;
-    int height=HEIGHT/5;
+public class FourBalls extends PApplet {
+
+    private static final int HEIGHT = 720;
+    private static final int WIDTH = 1280;
+
     public static void main(String[] args) {
-        PApplet.main("FourBalls",args);
+        PApplet.main(new String[]{"FourBalls"});
     }
+
 
     @Override
     public void settings() {
-        super.settings();
         size(WIDTH, HEIGHT);
     }
+
     @Override
     public void setup() {
+        ArrayOfObjectCreation();
+    }
+    int x=1,i;
+    int height = HEIGHT/5;
+    int speed=1;
+    ArrayList<FourBall> arr = new ArrayList<FourBall>();
+    private void ArrayOfObjectCreation() {
+        for(i=0;i<4;i++){
+            arr.add(new FourBall(x,speed*height,speed));
+            speed++;
+        }
     }
 
     @Override
     public void draw() {
-
-        drawCircle1();
-        drawCircle2();
-        drawCircle3();
-        drawCircle4();
-        x++;
+        for(FourBall ele : arr){
+            ele.draw();
+        }
     }
-
-    private void drawCircle4() {
-        ellipse(4*x,4*height,50,50);
-    }
-
-    private void drawCircle3() {
-        ellipse(3*x,3*height,50,50);
-    }
-
-    private void drawCircle2() {
-        ellipse(2*x,2*height,50,50);
-    }
-
-    private void drawCircle1() {
-        ellipse(x,height,50,50);
+    public class FourBall{
+        int x,y,speed;
+        public FourBall(int x,int y,int speed){
+            this.x=x;
+            this.y=y;
+            this.speed=speed;
+        }
+        void draw(){
+            ellipse(x*speed, y,50,50);
+            this.x=x+1;
+        }
     }
 }
